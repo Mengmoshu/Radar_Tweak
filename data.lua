b89bb6dcdbb2e2bbe46c0a1dee19b48efe7a6a96
Mengmoshu <mengmoshu@gmail.com>
2016-10-23 09:06:59 -0700

    The beginning of time.

---
+++ data.lua
@@ -0,0 +1,4 @@
+-- Make minimal changes to the radar prototype
+data.raw.radar["radar"].max_distance_of_sector_revealed = 28
+data.raw.radar["radar"].max_distance_of_nearby_sector_revealed = 6
+data.raw.radar["radar"].energy_per_nearby_scan = "250kJ"
---
+++ readme.md
@@ -0,0 +1,1 @@
+Extends the range of normal Radars, and increases their energy cost. Forked from Radarplus for RSO.
